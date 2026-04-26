2cc28eca1c153c5dc3f926e7219ddf91c8619a92
Miguel Angel Tobón Morales <miguela.tobonm@uqvirtual.edu.co>
2026-04-25 22:53:36 -0500

    Change module instantiation in tb.v
    Updated module instantiation to use 'tt_um_alu_7bits'.

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_alu_7bits user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
